093b2561774007560291ed2000e1cf788ac5cd5e
adelalira <aalira.96@gmail.com>
2022-06-11 18:45:19 +0200

    arreglo url git

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	/**
 	 * Variable que contiene la url del Front end
 	 */
-	static final String urlFront = "https://adelalira.github.io/";
+	static final String urlFront = "https://adelalira.github.io";
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {

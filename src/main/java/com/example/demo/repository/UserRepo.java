610795e1c1568bc18749983eadbe59e196c9828d
lauramolez <lauramorelez@gmail.com>
2022-06-12 21:03:06 +0200

    z

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -30,8 +30,8 @@ public interface UserRepo extends JpaRepository<User, Long> {
 	 * @param busqueda
 	 * @return
 	 */
-	@Query(value = "SELECT * FROM usuario WHERE AND id != :idUser ", nativeQuery = true)
-	public List<User> findByUsername( Long idUser);//username LIKE %:username%
+	@Query(value = "SELECT * FROM usuario WHERE id != :idUser ", nativeQuery = true)
+	public List<User> findByUsername( Long idUser);//AND username LIKE %:username%
 	
 	/**
 	 * Selecciona a los usuarios cuyos usernames coincidan con la búsqueda introducida y sean amigos del usuario que realiza la búsqueda.

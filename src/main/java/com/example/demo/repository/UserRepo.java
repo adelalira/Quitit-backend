45dcfe9efa3ac0ab0f9206de759bb5bb63802ed4
lauramolez <lauramorelez@gmail.com>
2022-06-15 19:49:40 +0200

    modificaciones para eliminar usuario

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -96,7 +96,7 @@ public interface UserRepo extends JpaRepository<User, Long> {
 	 * @param idUser
 	 */
 	@Modifying //declare that it is an update query and you don't expect a result back from the DB
-	@Query(value="DELETE FROM usuario_friends WHERE user_id = ?1 OR friends_id = ?1", nativeQuery = true)
+	@Query(value="DELETE FROM usuario_friends WHERE friends_id = ?1", nativeQuery = true)
 	public void deleteUserFriends(Long idUser);
 
 	/**
